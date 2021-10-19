f1cd64cee0a4bff13a89ad6940119154319d02d0
Ola <ola.cieszynska@gmail.com>
2021-10-19 21:56:38 +0200

    Init Commit

---
+++ src/main/java/com/ola/empik/ZadanieEmpikApplication.java
@@ -0,0 +1,13 @@
+package com.ola.empik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieEmpikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadanieEmpikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ola/empik/ZadanieEmpikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ola.empik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieEmpikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
